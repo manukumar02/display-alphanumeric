1c03359615ff0e7d9ee5e6dadd38b392e1940ce2
Manu Sharma <manukumar02@gmail.com>
2021-07-20 20:19:54 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'MIHIT';
+  randomCharacter = 'A';
+
+
+  generateRandomCharacter(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+
+
+    this.randomCharacter = result;
+  }
+}
